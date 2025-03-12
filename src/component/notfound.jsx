232205ca614b9c661c6ac1d2c3f0a91d7eb4c5ea
bassinebrabije <brabije.yassine123@gmail.com>
2025-03-12 19:44:15 +0000

    fix 400 page

--- src/component/notfound.jsx
+++ src/component/notfound.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const ErrorComponent = () => {
     return (
-        <div className="w-full flex-col justify-start items-center lg:gap-16 gap-10 inline-flex lg:pt-[180px] pt-12 lg:pb-28 pb-12">
+        <div className="w-full flex-col p-8 justify-start items-center lg:gap-16 gap-10 inline-flex lg:pt-[150px] pt-32 lg:pb-28 pb-12">
             <div className="relative h-[calc(550px-85px)] flex justify-center items-center flex-col">
                 <img src="/Oops-404-robot-cuate.svg" alt="Lost in Space" className="w-80 h-auto" />
                 <div className="block text-center mt-5">
-                    <h5 className="md:text-3xl text-lg leading-8 text-gray-900 font-bold mb-1.5">
+                    <h5 className="md:text-3xl text-2xl leading-8 text-gray-900 font-bold mb-1.5">
                         <span className="text-[#2a6171] mb-20">Uh-oh! </span>  Looks like you took a wrong turnip.
-                        <div className="pt-4"></div>  {/* Adds padding above the line */}
+                        <div className="pt-4 sm:block hidden"></div>  {/* Adds padding above the line */}
                         This page is un-bee-lievably missing!
                     </h5>
                     <div className="pb-4"></div>
